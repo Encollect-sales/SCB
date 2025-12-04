015cc70b25ba4645261c6b020f06a36bbd17230f
Arunkumars26 <arun.sakthivel@apptestify.com>
2025-12-04 14:13:54 +0530

    Execution Completed Nov-27 And Fixed P1 failed scripts

---
+++ October_Release_SCB/Cypress/e2e/tests/Allocation_UT_TestPage_13.cy.js
@@ -0,0 +1,29 @@
+import { readExcelFile } from '../utils/excelutils';
+import { getTestData } from '../utils/testDataUtils';
+import LoginPage from '../pages/LoginPage';
+import { getLocators } from '../utils/locatorUtils';
+import AllocationPage from '../pages/AllocationPage';
+
+describe('Allocation - User Type -', () => {
+    let loginPage;
+    let allocationpage; 
+
+    before(() => {
+        // Load and set locators before any tests run
+        getLocators('loginPage').then(locators => {
+            loginPage = new LoginPage(locators);
+        });
+
+        getLocators('Allocation').then(locators => {  
+            allocationpage = new AllocationPage(locators); 
+        });
+    });
+
+    it('Allocation -User Type- TC_ID_13', () => {
+        getTestData('loginData', 'login').then(user => {
+            loginPage.login(user.Companyname, user.email, user.password);
+                cy.wait(2000);
+                allocationpage.Allocation_UT_TestPage_13();            
+            });
+        });
+    });
